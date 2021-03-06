830db73ab2a94593a00af82f559614229989214d
Andrey Klemenko <web.klemenko@gmail.com>
2021-03-07 01:37:29 +0200

    add debounce func

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './styles.css';
 import fetchCountries from '../src/js/fetchCountries';
+import templateForOne from './templates/template-for-one.hbs';
+import templateForAll from './templates/template-for-all.hbs';
+const _ = require('lodash');
+const inputRef = document.querySelector('#input-query');
+inputRef.addEventListener('input', _.debounce(onInputDelay, 500));
 
-fetchCountries('ukraine');
+function onInputDelay(event) {
+  const currentValue = event.target.value;
+  fetchCountries(currentValue);
+}
 
 //рендер запроса:
 //Name
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,6 @@
 import BASE_URL from './settings';
 function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}/${searchQuery}`).then(console.log);
+  return fetch(`${BASE_URL}/${searchQuery}`).then(data => data.json());
 }
 
 export default fetchCountries;
