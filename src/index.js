1daed24460b3122120a331becb0fc8667e9674fd
Andrey Klemenko <web.klemenko@gmail.com>
2021-03-07 15:38:42 +0200

    make render html,make function clear input...

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import templateForOne from './templates/template-for-one.hbs';
 import templateForAll from './templates/template-for-all.hbs';
 const _ = require('lodash');
 const inputRef = document.querySelector('#input-query');
+const countriesListRef = document.querySelector('.countries__list');
 inputRef.addEventListener('input', _.debounce(onInputDelay, 500));
 
 function onInputDelay(event) {
   const currentValue = event.target.value;
   fetchCountries(currentValue);
+  clearMarkup(currentValue);
 }
 
-//рендер запроса:
-//Name
-//Capital
-//Population
-//[languages:name]
-//flag:link
+// переделать!!
+function clearMarkup(event) {
+  if (event === '') {
+    countriesListRef.textContent = '';
+  }
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,36 @@
 import BASE_URL from './settings';
+import templateForAll from '../templates/template-for-all.hbs';
+import templateForOne from '../templates/template-for-one.hbs';
+const countriesListRef = document.querySelector('.countries__list');
+
+//!!
+// Сделать рефакторинг
+// Вінести функции
+// Переделать очистку инпута
+// прикрутить нотификашки
+//!!
+
 function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}/${searchQuery}`).then(data => data.json());
+  return fetch(`${BASE_URL}/${searchQuery}`)
+    .then(response => {
+      if (!response.ok) throw new Error('Не найдено');
+
+      return response.json();
+    })
+    .then(data => {
+      const markupAll = templateForAll(data);
+      const markupOne = templateForOne(data);
+      if (data.length > 1 && data.length < 10) {
+        insertMarkup(markupAll, countriesListRef);
+      }
+      if (data.length === 1) {
+        insertMarkup(markupOne, countriesListRef);
+      }
+    });
+}
+
+function insertMarkup(value, ref) {
+  ref.insertAdjacentHTML('beforeend', value);
 }
 
 export default fetchCountries;
